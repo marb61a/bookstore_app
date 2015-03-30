e24ac9404caaa4c3692caf465312c17342521bc8
Martin Byrne <marb61a@gmail.com>
2015-03-30 14:38:57 +0000

    Added index and show action specs

---
+++ spec/controllers/authors_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe AuthorsController do
+  describe "GET #index" do
+    
+    it "assigns the authors objects to @authors variable"do
+      author1 = Fabricate(:author)
+      author2 = Fabricate(:author)
+      
+      get :index
+      expect(assigns(:authors)).to match_array([author1, author2])
+    end
+    
+    it "renders the index template"do
+      get :index
+      expect(response).to render_template('index')
+    end
+  end
+  
+  describe "GET #show" do
+    let(:author) { Fabricate(:author) }
+    it "finds the author with the given id and assigns to @author variable" do
+      #author = Fabricate(:author)
+      get :show, id: author
+      expect(assigns(:author)).to eq(author)
+    end
+    
+    it "renders the show template" do
+      #author = Fabricate(:author)
+      get :show, id: author
+      expect(response).to render_template('show')
+    end
+    
+  end
+end
