4ff522afae63f155e13b6b7bcc65d1036e750e94
Martin Byrne <marb61a@gmail.com>
2015-03-31 14:40:27 +0000

    Added specs for the successful create action

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,12 +2,24 @@ class AuthorsController < ApplicationController
   def index
     @authors = Author.all
   end
+  
   def show
     @author = Author.find(params[:id])
     #require 'pry';binding.pry
   end
+  
   def new
     @author = Author.new
   end
   
+  def create
+    @author = Author.new(author_params)
+    @author.save
+    redirect_to @author
+  end
+  
+  private
+  def author_params
+    params.require(:author).permit(:first_name, :last_name)
+  end
 end
--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -37,6 +37,32 @@ describe AuthorsController do
       get :new
       expect(assigns(:author)).to be_instance_of(Author)
     end
+    
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template('new')
+    end
    end
+  
+   describe "POST #create" do
+     context "a successful create" do
+       it "saves the new author object" do
+         ##require 'pry';binding.pry
+         post :create, author: Fabricate.attributes_for(:author)
+         expect(Author.count).to eq(1)
+       end
+       it "redirects to the show action" do
+         post :create, author: Fabricate.attributes_for(:author)
+         expect(response).to redirect_to author_path(Author.first)
+       end
+       it "sets the success flush message" do
+       end
+     end
+     
+     context "an unsuccessful create" do
+       
+     end
+   end
+  
   
 end
