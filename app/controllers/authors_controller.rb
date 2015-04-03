4dc837e5be088ad7dea1163e8c0262eb0a4f1fe1
Martin Byrne <marb61a@gmail.com>
2015-04-03 14:28:16 +0000

    Added author controller specs

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,10 +1,11 @@
 class AuthorsController < ApplicationController
+  before_action :set_author, except: [:index, :new, :create]
+  
   def index
     @authors = Author.all
   end
   
   def show
-    @author = Author.find(params[:id])
     #require 'pry';binding.pry
   end
   
@@ -14,12 +15,42 @@ class AuthorsController < ApplicationController
   
   def create
     @author = Author.new(author_params)
-    @author.save
-    redirect_to @author
+    if @author.save
+      flash[:success] = 'Author has been created.'
+      redirect_to @author
+    else
+      flash[:success] = 'Author has not been created.'
+      render :new
+    end
+  end
+  
+  def edit
+  
+  end
+  
+  def update
+    if @author.update(author_params)
+      flash[:success] = "Author has been updated."
+      redirect_to @author
+    else
+      flash[:danger] = "Author has not been updated."
+      render :edit
+    end
+  end
+  
+  def destroy
+    if @author.destroy
+      flash[:success] = "Author has been deleted."
+      redirect_to authors_path
+    end
   end
   
   private
   def author_params
     params.require(:author).permit(:first_name, :last_name)
+  end
+  
+  def set_author
+    @author = Author.find(params[:id])
   end
 end
