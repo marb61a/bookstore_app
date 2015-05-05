f2c5d1d78b0782d7a4b2d5a1949f969e6369d02e
Martin <marb61a@gmail.com>
2015-05-05 14:42:08 +0000

    updated yaml file

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 20150420133742) do
 
   create_table "authors", force: true do |t|
