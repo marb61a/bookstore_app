dca03bee55424dc201e0d85276ce8a0f5f591992
Martin <marb61a@gmail.com>
2015-05-07 14:36:51 +0000

    Added partial and show user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ActiveRecord::Base
   has_many :orders
 
   validates :first_name, :last_name, :email, presence: true
-  validates :email, presence: true, uniqueness: { case_sensitive: false },
-    format: { with: /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i }
 
   has_secure_password
 
