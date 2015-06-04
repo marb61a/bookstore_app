9fad2438e53519543228930735cb83f29d4e0b44
Martin <marb61a@gmail.com>
2015-06-04 10:52:34 +0000

    adding password reset specs

---
+++ spec/features/admin/showing_books_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+feature 'Listing Books' do
+  let!(:book) { Fabricate(:book) }
+  let(:admin) { Fabricate(:admin) }
+  let(:user) { Fabricate(:user) }
+  
+  before do
+    sign_in_as admin
+  end
+  
+  scenario 'access to non-admins not allowed' do
+    deny_access_to_non_admins(user, 'Books')
+  end
+  
+  scenario 'successfully showing book details' do
+    visit root_path
+    click_link 'Books'
+    click_link book.title
+    expect(page).to have_content(book.title)
+    expect(current_path).to eq(admin_book_path(book.id))
+  end
+  
+  scenario 'failed listing of all books' do
+    visit root_path
+    click_link 'Sign Out'
+    visit admin_book_path(book.id)
+    expect(page).to have_content('Please sign in first!')
+    expect(current_path).to eq(signin_path)
+  end
+end
