38432ef8fc50f40901233c532655ce4ef97740ee
Martin <marb61a@gmail.com>
2015-06-08 10:41:46 +0000

    rapaired macros error

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -1,3 +1,11 @@
+def set_current_user(user=nil)
+  session[:user_id] = (user || Fabricate(:user)).id
+end
+
+def current_user
+  User.find(session[:user_id])
+end
+
 def set_admin_user(admin=nil)
   session[:user_id] = (admin || Fabricate(:admin)).id
 end
