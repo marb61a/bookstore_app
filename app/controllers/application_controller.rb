b86e057dfef3f4d5cc4c7cc3e75c6365c3608173
Martin <marb61a@gmail.com>
2015-06-08 18:08:40 +0000

    added files and successfully tested in firefox

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
 
 private
   def set_cart
-    #session[:cart_id] = nil
     if session[:cart_id]
       @cart = Cart.find session[:cart_id]
     else
--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -17,11 +17,8 @@ class CatalogsController < ApplicationController
   def search
     @books = Book.search_by_title(params[:search_word])
   end
-  
 private
-  
   def set_cart_if_session
     set_cart if session[:cart_id]
   end
-  
 end
